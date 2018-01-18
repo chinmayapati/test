1c0b04143cd52ddf885081192af00dcae8dddd33
Chinmaya Pati <chinmaya.cp@gmail.com>
2018-01-18 15:29:24 +0000

    Added to specified directory

---
+++ update.py
@@ -0,0 +1,28 @@
+import os, time
+
+while True:
+    # run an update
+    os.system("git pull")
+    
+    #change file locations
+    DIR = os.curdir
+    files = os.listdir(DIR)
+    
+    madeChanges = False
+    
+    for i in files:
+        madeChanges = True
+        if i[:-3]=="apk":
+            print i, DIR+"/android/"+i
+            #os.rename(i, DIR+"/android/"+i)
+        else:
+            print i, DIR+"/ios/"+i
+            #os.rename(i, DIR+"/ios/"+i)
+    
+    if madeChanges:
+        os.system("git add .")
+        os.system("git commit -m \"Added to specified directory\"")
+        os.system("git push")
+        
+    time.sleep(5)
+
