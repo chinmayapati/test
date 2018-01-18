685d612fd6ce7375b696fb9dcecaf67f1d897146
Chinmaya Pati <chinmaya.cp@gmail.com>
2018-01-18 15:30:04 +0000

    Added to specified directory

--- update.py
+++ update.py
@@ -20,9 +20,12 @@ while True:
             #os.rename(i, DIR+"/ios/"+i)
     
     if madeChanges:
+        print "\n\nFound some changes!"
         os.system("git add .")
         os.system("git commit -m \"Added to specified directory\"")
         os.system("git push")
+    else:
+        print "\n\nNo new changes :)"
         
     time.sleep(5)
 
