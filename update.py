0b23e37ee988d6734d58019cfa651cc47b5819da
Chinmaya Pati <chinmaya.cp@gmail.com>
2018-01-18 17:59:48 +0000

    deleted kar file and added ios dir

--- update.py
+++ update.py
@@ -2,32 +2,36 @@ import os, time
 
 DIR = os.curdir
 
-while True:
-    
-    # run an update
-    os.system("git pull")
-    
-    #change file locations
-    files = os.listdir(DIR)
-    
-    madeChanges = False
-    
-    for i in files:
-        if i[-3:]=="apk":
-            madeChanges = True
-            os.rename(i, DIR+"/android/"+i)
-        elif i[-3:] in ["KAR","ipa"]:
-            madeChanges = True
-            os.rename(i, DIR+"/ios/"+i)
-    
-    if madeChanges:
-        print "\n","="*30,"\nFound some changes! :(\n"
-        os.system("git add .")
-        os.system('git commit -m "Added files to specified directory"')
-        os.system("git push")
-        print "\n","="*30,"\nChanges settled! :)\n"
-    
-    else:
-        print "\nNo new changes :)\n"
+try:
+    while True:
         
-    time.sleep(5)
+        # run an update
+        os.system("git pull")
+        
+        #change file locations
+        files = os.listdir(DIR)
+        
+        madeChanges = False
+        
+        for i in files:
+            if i[-3:]=="apk":
+                madeChanges = True
+                os.rename(i, DIR+"/android/"+i)
+            elif i[-3:] in ["KAR","ipa"]:
+                madeChanges = True
+                os.rename(i, DIR+"/ios/"+i)
+        
+        if madeChanges:
+            print "\n","="*30,"\nFound some changes! :(\n"
+            os.system("git add .")
+            os.system('git commit -m "Added files to specified directory"')
+            os.system("git push")
+            print "\n","="*30,"\nChanges settled! :)\n"
+        
+        else:
+            print "\nNo new changes :)\n"
+            
+        time.sleep(5)
+
+except KeyboardInterrupt as ki:
+    print "\nExiting!\n"
