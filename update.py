3c3009dcbea9b5fdae1d021bca06e27070337b11
Chinmaya Pati <chinmaya.cp@gmail.com>
2018-01-18 17:28:58 +0000

    modified update method

--- update.py
+++ update.py
@@ -11,22 +11,20 @@ while True:
     madeChanges = False
     
     for i in files:
-        if i[:-3]=="apk":
+        if i[-3:]=="apk":
             madeChanges = True
-            print i, DIR+"/android/"+i
-            #os.rename(i, DIR+"/android/"+i)
-        elif i[:-3] in ["KAR","ipa"]:
+            os.rename(i, DIR+"/android/"+i)
+        elif i[-3:] in ["KAR","ipa"]:
             madeChanges = True
-            print i, DIR+"/ios/"+i
-            #os.rename(i, DIR+"/ios/"+i)
+            os.rename(i, DIR+"/ios/"+i)
     
     if madeChanges:
-        print "\n\nFound some changes!"
+        print "\n","="*15,"\nFound some changes!"
         os.system("git add .")
-        os.system("git commit -m \"Added to specified directory\"")
+        os.system('git commit -m "Added files to specified directory"')
         os.system("git push")
     else:
-        print "\n\nNo new changes :)"
+        print "\nNo new changes :)"
         
     time.sleep(5)
 
